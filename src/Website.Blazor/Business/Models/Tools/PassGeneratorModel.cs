af8578faac584cc1cd016d51873e6043d909abc0
Naser Rafinia <Naser.r@gmail.com>
2022-02-21 16:28:20 +0330

    Add Password generator

---
+++ src/Website.Blazor/Business/Models/Tools/PassGeneratorModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Business.Models.Tools
+{
+    public class PassGeneratorModel
+    {
+        [Display(Name = "Include Uppercase Characters (A-Z)")]
+        public Boolean UpperCase { get; set; }
+
+        [Display(Name = "Include Lowercase Characters (a-z)")]
+        public Boolean LowerCase { get; set; }
+
+        [Display(Name = "Include Numbers (1234567890)")]
+        public Boolean Numberic { get; set; }
+
+        [Display(Name = "Include Symbols (*$-+?_&=!%{}/)")]
+        public Boolean SpecialChar { get; set; }
+
+        [Display(Name = "Custom Characters")]
+        public Boolean CustomChar { get; set; }
+
+        [Display(Name = "Custom Characters")]
+        [DataType(DataType.Text)]
+        public string Custom { get; set; }
+
+        [Display(Name = "Password Length")]
+        public int Len { get; set; }
+
+        [Display(Name = "Prefix")]
+        [DataType(DataType.Text)]
+        public string Prefix { get; set; }
+
+        [Display(Name = "Suffix")]
+        [DataType(DataType.Text)]
+        public string Suffix { get; set; }
+    }
+}
---
+++ src/Website.Blazor/Business/Services/Tools/PassGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Business.Models.DI;
+using Business.Models.Tools;
+using MlkPwgen;
+
+namespace Business.Services.Tools
+{
+    public class PassGenerator : ISingleton
+    {
+        private const string CharsLCase = "abcdefgijkmnopqrstwxyz";
+        private const string CharsUCase = "ABCDEFGHJKLMNPQRSTWXYZ";
+        private const string CharsNumeric = "0123456789";
+        private const string CharsSpecial = "*$-+?_&=!%{}/";
+
+        public string Generate(PassGeneratorModel model)
+        {
+            var pLen = model.Prefix?.Length ?? 0;
+            var sLen = model.Suffix?.Length ?? 0;
+            if (pLen + sLen > model.Len)
+            {
+                return model.Prefix + model.Suffix;
+            }
+
+            var len = model.Len - pLen - sLen;
+            var passStr = string.Empty;
+            if (model.LowerCase)
+                passStr += CharsLCase;
+            if (model.UpperCase)
+                passStr += CharsUCase;
+            if (model.Numberic)
+                passStr += CharsNumeric;
+            if (model.SpecialChar)
+                passStr += CharsSpecial;
+
+            if (!string.IsNullOrWhiteSpace(model.Custom))
+                passStr = model.Custom;
+
+            if (string.IsNullOrWhiteSpace(passStr))
+                return string.Empty;
+
+            var res = PasswordGenerator.Generate(len, passStr);
+            res = $"{model.Prefix}{res}{model.Suffix}";
+            return res;
+        }
+    }
+}
